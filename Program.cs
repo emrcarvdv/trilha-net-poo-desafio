516cb12d6fc8973b77645f6f61cf413cdf94e2b0
Emerson Carvalho <128149335+emrcarvdv@users.noreply.github.com>
2023-11-03 11:10:25 -0300

    feat; done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+Smartphone iphone = new Iphone("987654321", "Iphone 11", "987654321", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
